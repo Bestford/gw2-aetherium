bd42f296901938ff390243862fb6a4bdb9c05230
adam.kelsall <adam.kelsall@accenture.com>
2015-10-31 19:08:11 +0000

    Added Angular form validation to only accept integers. Button is not shown unless valid

--- js/controllers.js
+++ js/controllers.js
@@ -4,5 +4,15 @@ angular
   .module('Gw2Aetherium.controllers', [])
 
   // Main and only controller
-  .controller('Main', function mainController() {
+  .controller('Main', function mainController($scope) {
+
+    // Define scope variables. Undefined variables prevent hoisiting
+    $scope.current;
+    $scope.target;
+
+    // ngPattern Regex used to prevent use of floats by the user
+    $scope.numberValidation = /^\d*$/;
+
+    // Maximum Aetherium capacity. Change this if a future update adds a new capacity upgrade
+    $scope.max = 3000;
   });
